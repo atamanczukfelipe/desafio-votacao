36b2640b27968a840bfcc8620c22cb155a5bf007
Felipe Atamanczuk <atamanczuk.felipe@gmail.com>
2025-05-19 16:06:23 -0300

    Pauta Controller

---
+++ src/main/java/br/com/votacao/controller/PautaController.java
@@ -0,0 +1,15 @@
+package br.com.votacao.controller;
+
+import br.com.votacao.dto.request.CriarPautaRequest;
+import br.com.votacao.model.Pauta;
+import br.com.votacao.service.PautaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/pautas")
+public class PautaController {
+    
+}
