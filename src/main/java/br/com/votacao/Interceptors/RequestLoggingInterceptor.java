57bbdf425b54c05edf5dc1960c8039524915b2c3
Felipe Atamanczuk <atamanczuk.felipe@gmail.com>
2025-05-20 15:21:51 -0300

    RequestLogginInterceptor.java

---
+++ src/main/java/br/com/votacao/Interceptors/RequestLoggingInterceptor.java
@@ -0,0 +1,39 @@
+package br.com.votacao.Interceptors;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.util.UUID;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Component
+public class RequestLoggingInterceptor implements HandlerInterceptor {
+
+    private static final Logger logger =  LoggerFactory.getLogger(RequestLoggingInterceptor.class);
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler){
+        String requestId = UUID.randomUUID().toString();
+        MDC.put("requestId", requestId);
+
+        String ip = request.getRemoteAddr();
+        String method = request.getMethod();
+        String uri = request.getRequestURI();
+        String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+
+        logger.info("[{}] {} {} from IP: {}", timestamp, method, uri, ip);
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex){
+        MDC.clear();
+    }
+}
