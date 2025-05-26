fd3da83d3db5f8ebdf0373a7f31a26eae7c12d0d
Felipe Atamanczuk <atamanczuk.felipe@gmail.com>
2025-05-26 09:18:26 -0300

    Adicionado Cosntrutor

--- src/main/java/br/com/votacao/dto/request/CriarPautaRequest.java
+++ src/main/java/br/com/votacao/dto/request/CriarPautaRequest.java
@@ -8,4 +8,9 @@ public class CriarPautaRequest {
     @NotBlank
     private String titulo;
     private String descricao;
+
+    public CriarPautaRequest(String titulo, String descricao){
+        this.titulo = titulo;
+        this.descricao = descricao;
+    }
 }
