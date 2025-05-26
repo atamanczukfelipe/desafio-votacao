dabec0aee4a82126486bc11e850190fc5e8a4543
Felipe Atamanczuk <atamanczuk.felipe@gmail.com>
2025-05-26 09:31:15 -0300

    Pauta Service Test Class

--- src/test/java/br/com/votacao/service/PautaServiceTest.java
+++ src/test/java/br/com/votacao/service/PautaServiceTest.java
@@ -4,6 +4,7 @@ import br.com.votacao.dto.request.CriarPautaRequest;
 import br.com.votacao.model.Pauta;
 import br.com.votacao.repository.PautaRepository;
 
+import org.checkerframework.checker.units.qual.C;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
@@ -24,4 +25,51 @@ class PautaServiceTest {
     @Mock
     private PautaRepository pautaRepository;
 
+    @Test
+    void criarPautaComSucesso(){
+
+        CriarPautaRequest request = new CriarPautaRequest("Pauta Teste", "Teste Descrição");
+
+        Pauta pautaMock = Pauta.builder()
+                                .id(1L)
+                                .titulo("Pauta Teste")
+                                .descricao("Teste Descrição")
+                                .build();
+
+        when(pautaRepository.save(any(Pauta.class))).thenReturn(pautaMock);
+
+        Pauta result = pautaService.criarPauta(request);
+
+        assertNotNull(result);
+        assertEquals("Pauta Teste", result.getTitulo());
+        verify(pautaRepository, times(1)).save(any());
+    }
+
+    @Test
+    void buscarPorIdRetornaSucesso(){
+
+        Long pautaId = 1L;
+        Pauta pauta = new Pauta(pautaId,"Reforma", "Teste", null);
+
+        when(pautaRepository.findById(pautaId)).thenReturn(Optional.of(pauta));
+
+        Pauta resutlado = pautaService.buscarPorId(pautaId);
+
+        assertNotNull(resutlado);
+        assertEquals(pautaId,resutlado.getId());
+    }
+
+    @Test
+    void buscarPorIdRetornaException(){
+
+        Long pautaId = 100L;
+
+        when(pautaRepository.findById(pautaId)).thenReturn(Optional.empty());
+
+        IllegalArgumentException ex = assertThrows(IllegalArgumentException.class, ()->{
+            pautaService.buscarPorId(pautaId);
+        });
+
+        assertEquals("Pauta Não Encontrada: " + pautaId, ex.getMessage());
+    }
 }
